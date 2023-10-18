1ad7730af50e7c486303e429f57a72b3dda914ae
rexxDigital <hello@rexx.icu>
2023-10-18 13:56:15 +0200

    Add files

---
+++ fast.js
@@ -0,0 +1,31 @@
+const { performance } = require('perf_hooks')
+
+function binary_search(v, beginning, end, needle) {
+  const start = v.at(beginning);
+  const middle = beginning + ((end - beginning) / 2);
+
+  if(needle == middle) {
+    return middle;
+  }
+
+  if(needle > middle) {
+    return binary_search(v, middle + 1, end, needle)
+  } else {
+    return binary_search(v, start, middle, needle)
+  }
+}
+
+let list = []
+
+for(let i = 0; i < 10000; ++i) {
+  list.push(i)
+}
+
+var startTime = performance.now()
+
+let meow = binary_search(list, 0, 9999, 7788)
+
+var endTime = performance.now()
+
+console.log(`found: ${meow}`)
+console.log(`${endTime - startTime} milliseconds`)
---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <chrono>
+#include <iostream>
+
+int binary_search(int haystack[], int needle, int start, int end) {
+  int middle = start + ((end - start) / 2);
+  int v = haystack[middle];
+
+  if(needle == v) {
+    return v;
+  }
+
+  if(needle > v) {
+    return binary_search(haystack, needle, middle + 1, end);
+  } else {
+    return binary_search(haystack, needle, start, middle);
+  }
+} 
+
+int main() {
+  int v = 2;
+  int arr[10000];
+
+  for(int i = 0; i < 10000; ++i) {
+    arr[i] = i;
+  }
+
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+  int meow = binary_search(arr, 7788, 0, 10000);
+
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+  std::cout << "Found: " << meow << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << "[ms]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+}
